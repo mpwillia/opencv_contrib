a96ca96b6f6684f279f1b90c4914f412c71862e9
Michael Williams <mike@clwill.com>
2015-10-12 20:55:10 +0000

    fixed the debugging messages

--- modules/face/src/lbph_faces.cpp
+++ modules/face/src/lbph_faces.cpp
@@ -387,7 +387,7 @@ void LBPH::train(InputArrayOfArrays _in_src, InputArray _in_labels, bool preserv
         Mat lbp_image = elbp(src[sampleIdx], _radius, _neighbors);
 
          if(sampleIdx == 0)
-            std::cout << "lbp_image size = " << lbp_image.cols() << "x" << lbp_image.rows() << " | depth = " << lbp_image.depth() << " | channels = " << lbp_image.channels() << "\n";
+            std::cout << "lbp_image size = " << lbp_image.cols << "x" << lbp_image.rows << " | depth = " << lbp_image.depth() << " | channels = " << lbp_image.channels() << "\n";
          
         // get spatial histogram from this lbp image
         Mat p = spatial_histogram(
@@ -398,7 +398,7 @@ void LBPH::train(InputArrayOfArrays _in_src, InputArray _in_labels, bool preserv
                 true);
 
          if(sampleIdx == 0)
-            std::cout << "p size = " << p.cols() << "x" << p.rows() << " | depth = " << p.depth() << " | channels = " << p.channels() << "\n";
+            std::cout << "p size = " << p.cols << "x" << p.rows << " | depth = " << p.depth() << " | channels = " << p.channels() << "\n";
 
         // add to templates
         _histograms.push_back(p);
