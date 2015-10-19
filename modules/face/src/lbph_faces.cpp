f9263043f1842fa2db3f554fe1d14ce4e3d101e2
Michael Williams <mike@clwill.com>
2015-10-19 08:44:57 +0000

    trying to load yaml and bin hists

--- modules/face/src/lbph_faces.cpp
+++ modules/face/src/lbph_faces.cpp
@@ -186,7 +186,7 @@ void LBPH::loadTest(const String &parent_dir, const String &modelname) {
     
     String histograms_dir(model_dir + "/" + modelname + "-histograms");
     for(size_t i = 0; i < labels.size(); i++) {
-        std::cout << "loading label " << labels.at((int)i) + "\n";
+        std::cout << "loading label '" << labels.at((int)i) + "'\n";
         /*
         String histfilename_base(histograms_dir + "/" + modelname + "-" + labels.at((int)label));
         String histfilename_yaml(histfilename_base + ".yml");
