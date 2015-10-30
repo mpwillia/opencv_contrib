a788e7640f851e7c1b922fefe96c68b3294ed450
Michael Williams <mike@clwill.com>
2015-10-30 00:35:06 +0000

    hmm, why different predictions?

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -876,12 +876,12 @@ void xLBPH::predict(InputArray _src, int &minClass, double &minDist) const {
     //int labelcount = 0;
     for(std::map<int, std::vector<Mat> >::const_iterator it = _histograms.begin(); it != _histograms.end(); ++it) {
         
-        std::cout << it->first << ", ";
         std::vector<Mat> hists = it->second;
 
         for(size_t histIdx = 0; histIdx < hists.size(); histIdx++) {
             double dist = compareHist(hists.at(histIdx), query, COMP_ALG);
             if((dist < minDist) && (dist < _threshold)) {
+                std::cout << "[" << it->first << " | " << dist << "], ";
                 minDist = dist;
                 minClass = it->first;
                 preds.push_back(it->first);
