5f8b2333d8d900f9e4434f31b6093dda5ab3e345
Michael Williams <mike@clwill.com>
2015-10-30 02:07:10 +0000

    hmm, why different predictions?

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -407,7 +407,7 @@ void xLBPH::mmapHistograms() {
         //struct stat st;
         //stat(filename.c_str(), &st);
         //char* mapPtr = (char*)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
-        char* mapPtr = (char*)mmap(NULL, getHistogramSize() * it->second * sizeof(float), PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+        char* mapPtr = (char*)mmap(NULL, getHistogramSize() * it->second * sizeof(float), PROT_READ, MAP_PRIVATE, fd, 0);
         if(mapPtr == MAP_FAILED)
             CV_Error(Error::StsError, "Cannot mem map file '"+filename+"'");
 
