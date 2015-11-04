2934c79e68a3140e4e2006933c1db490d11fe298
Michael Williams <mike@clwill.com>
2015-11-04 01:16:27 +0000

    attempting multithreading

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -931,6 +931,9 @@ void xLBPH::calculateHistograms_multithreaded(const std::vector<Mat> &images, st
         std::vector<std::vector<Mat> > splitHistsDst;
         std::vector<std::thread> threads;
         for(int i = 0; i < numThreads; i++) {
+            //std::vector<Mat> threadImages = splitImages.at(i);
+            std::vector<Mat> threadHistsDst;
+            splitHistsDst.push_back(threadHistsDst);
             threads.push_back(std::thread(&xLBPH::calculateHistograms_multithreaded, this, std::ref(splitImages.at(i)), std::ref(splitHistsDst.at(i)), false));
         }
 
