a91aa7e5872f098e0c70952bc135e7bf7f9a490d
Michael Williams <mike@clwill.com>
2015-11-07 00:44:59 +0000

    trying template multithreading function

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -515,7 +515,7 @@ void xLBPH::averageHistograms(const std::vector<Mat> &hists, Mat &histavg) const
     histavg.convertTo(histavg, CV_32FC1);
 }
 
-void xLBPH::calcHistogramAverages_thread(std::vector<int> labels, std::vector<Mat> avgsdst) const {
+void xLBPH::calcHistogramAverages_thread(const std::vector<int> labels, std::vector<Mat> avgsdst) const {
     for(size_t idx = 0; idx < labels.size(); idx++) {
         Mat histavg;
         averageHistograms(_histograms.at(labels.at(idx)), histavg);
@@ -529,7 +529,7 @@ bool xLBPH::calcHistogramAverages() const {
 
     std::vector<Mat> averages;
     std::vector<int> labels; 
-    for(std::map<int,int>::const_iterator it = _labelinfo.begin(); it != _labelinfo.end(); i++)
+    for(std::map<int,int>::const_iterator it = _labelinfo.begin(); it != _labelinfo.end(); it++)
         labels.push_back(it->first);
     
     //performMultithreadedCalc(const std::vector<S> &src, std::vector<D> &dst, int numThreads, void (xLBPH::*calcFunc)(const std::vector<S> &src, std::vector<D> &dst)) {
