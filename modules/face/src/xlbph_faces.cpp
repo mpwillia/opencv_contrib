1cbc7663b2780328fd37b38db5c22611ffb665e9
Michael Williams <mike@clwill.com>
2015-11-10 22:01:24 +0000

    testing MCL clustering

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -787,6 +787,10 @@ void xLBPH::clusterHistograms() {
         printf("Inverted Normalized Probs:\n");
         printMat(mclmat, it->first);
 
+        mcl_inflate(mclmat, mcl_inflation_power);
+        printf("Inverted Normalized Inflated Probs:\n");
+        printMat(mclmat, it->first);
+
         // perform mcl inflation iterations
         for(int i = 0; i < mcl_iterations; i++) {
             mcl_expand(mclmat, mcl_expansion_power);
