9e31f468517e2ec658acb920596f8cc846c13390
Michael Williams <mike@clwill.com>
2015-11-10 00:56:13 +0000

    testing MCL clustering

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -648,8 +648,9 @@ void xLBPH::printMat(const Mat &mat, int label) const {
 static void mcl_normalize(Mat &src) {
     for(int i = 0; i < src.cols; i++) {
         Mat col = src.col(i);
-        double sum = (int)sum(col);
-        col /= sum;
+        col /= sum(col);
+        //double s = (int)sum(col);
+        //col /= s;
         //in theory col shares data with src so this should be all we need to do
     } 
 }
