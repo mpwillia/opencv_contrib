ff84d002b479b1d7d5e44341b36b96f83bdeb2e2
Michael Williams <mike@clwill.com>
2015-11-11 05:14:09 +0000

    testing MCL clustering

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -852,6 +852,7 @@ void xLBPH::clusterHistograms() {
 
         // initial normalization
         mcl_normalize(mclmat);
+        mcl_prune(mclmat, mcl_prune_min);
         printf("Normalized:\n");
         printMat(mclmat, it->first);
 
