a6ea2e9125d01cbe19f97430d6fe5d1a6d66c754
Michael Williams <mike@clwill.com>
2015-11-21 20:09:22 +0000

    attempting to seperate out MCL code from xLBPH code

--- modules/face/src/xlbph_faces.cpp
+++ modules/face/src/xlbph_faces.cpp
@@ -171,9 +171,9 @@ private:
     void mcl_inflate(Mat &src, double power);
     void mcl_prune(Mat &src, double min);
     void mcl_iteration(Mat &src, int e, double r, double prune); // Performs one MCL iteration of expand -> inflate -> prune
-    */
     void mcl_converge(Mat &src, int e, double r, double prune); // Performs MCL iterations until convergence
     void mcl_cluster(Mat &src, int iters, int e, double r, double prune); // Perfoms either <iters> MCL iterations or iterates until convergence
+    */
 
     // Markov Clustering Algorithm (MCL)- Settings
     /* Sets the number of MCL iterations, default is 10
