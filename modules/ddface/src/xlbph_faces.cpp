f877a6a3241b4fcb31ae03aeb84ebfeafbe59884
Michael Williams <mike@clwill.com>
2015-11-24 03:27:02 +0000

    attempting to build with TBB

--- modules/ddface/src/xlbph_faces.cpp
+++ modules/ddface/src/xlbph_faces.cpp
@@ -985,7 +985,7 @@ void xLBPH::clusterHistograms() {
 
 
     //double avgCheckRatio = 0;
-    tbb::parallel_for(tbb::blocked_range(_histograms.being(), _histograms.end()), [=](std::map<int, std::vector<Mat>>::const_iterator it) {
+    tbb::parallel_for(tbb::blocked_range(_histograms.begin(), _histograms.end()), [=](std::map<int, std::vector<Mat>>::const_iterator it) {
     //for(std::map<int, std::vector<Mat>>::const_iterator it = _histograms.begin(); it != _histograms.end(); it++) {
         
         int numHists = (int)it->second.size();
@@ -1026,7 +1026,7 @@ void xLBPH::clusterHistograms() {
     
         //break;
         */
-    });
+    }, tbb::auto_partitioner());
     /*
     avgCheckRatio /= (int)_histograms.size();
     printf("\n### Overall ###\n");
