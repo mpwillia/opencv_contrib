3732a10266909829dac6e69a83475cb5b8ce171b
Michael Williams <mike@clwill.com>
2016-02-13 15:27:49 -0800

    updated modelstorage to add more helper function along with basic read/writing histograms

--- modules/ddface/src/xlbph_faces.cpp
+++ modules/ddface/src/xlbph_faces.cpp
@@ -126,8 +126,8 @@ private:
     int minLabelsToCheck = 10;
     double labelsToCheckRatio = 0.05;
 
-    int minClustersToCheck = 5;
-    double clustersToCheckRatio = 0.5;
+    int minClustersToCheck = 3;
+    double clustersToCheckRatio = 0.25;
 
     //void predict_cluster(InputArray _src, int &label, double &dist) const;
 
@@ -812,6 +812,7 @@ void xLBPH::clusterHistograms() {
                                     mcl_expansion_power, 
                                     mcl_inflation_power, 
                                     mcl_prune_min};
+
         std::vector<clstr::cluster_t> labelClusters;
         clstr::clusterHistograms(_histograms[it.first], labelClusters, vars);
 
