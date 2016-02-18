441ddb78633cc6c9c590c5482a3254e9cc26e2d2
Michael Williams <mike@clwill.com>
2016-02-17 19:29:54 -0800

    changed clusters to reference histograms by index rather than as a mat reference

--- modules/ddface/src/xlbph_faces.cpp
+++ modules/ddface/src/xlbph_faces.cpp
@@ -1605,7 +1605,8 @@ void xLBPH::predict_avg_clustering(InputArray _query, tbb::concurrent_vector<std
     }
     */
 
-    tbb::concurrent_vector<std::pair<int, std::vector<Mat>>> labelhists;
+    //tbb::concurrent_vector<std::pair<int, std::vector<Mat>>> labelhists;
+    tbb::concurrent_vector<std::pair<int, clusters::idx_cluster_t>> labelhists;
     tbb::parallel_for(0, numLabelsToCheck, 1, 
         [&](int i) {
             
@@ -1651,7 +1652,7 @@ void xLBPH::predict_avg_clustering(InputArray _query, tbb::concurrent_vector<std
     // check best labels by cluster
     tbb::concurrent_vector<std::pair<int, std::vector<double>>> labeldists;
     tbb::parallel_for_each(labelhists.begin(), labelhists.end(),
-        [&labelhists, &labeldists, &query](std::pair<int, std::vector<Mat>> it) {
+        [&labelhists, &labeldists, &query, &_histograms](std::pair<int, std::vector<Mat>> it) {
             
             cluster::idx_cluster_t cluster = it.second;
             std::vector<Mat> allHists = _histograms.at(it.first).second;
