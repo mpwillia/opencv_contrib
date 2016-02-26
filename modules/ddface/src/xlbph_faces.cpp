5c87381079cfde17f87c0818bcd5c24df690977a
Michael Williams <mike@clwill.com>
2016-02-25 20:04:44 -0800

    adjusting clusters to check ratio

--- modules/ddface/src/xlbph_faces.cpp
+++ modules/ddface/src/xlbph_faces.cpp
@@ -127,7 +127,7 @@ private:
     double labelsToCheckRatio = 0.05;
 
     int minClustersToCheck = 3;
-    double clustersToCheckRatio = 0.10;
+    double clustersToCheckRatio = 0.20;
 
     //void predict_cluster(InputArray _src, int &label, double &dist) const;
 
